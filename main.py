8268fabcf59bb9201aa0d986b490b2a489271f09
Jenny Chen <shijiajchen@gmail.com>
2023-01-25 20:44:04 -0500

    WHY

--- main.py
+++ main.py
@@ -24,94 +24,94 @@ table = table[0].tbody.contents
 
 all_players = {}
 
-for row in table:
-  contents = row.contents
-  week = contents[4].text
-  if week[:-1] == "WEEK":
-    week = int(week[-1])
-    game_url = base_url + str(contents[0].a['href'])[2:-5] + "fullstats/"
-    game_response = requests.get(game_url, headers=headers)
-    game_soup = BeautifulSoup(game_response.text, 'html.parser')
-    game_table = game_soup.find(class_ = "completestats")
+# for row in table:
+#   contents = row.contents
+#   week = contents[4].text
+#   if week[:-1] == "WEEK":
+#     week = int(week[-1])
+#     game_url = base_url + str(contents[0].a['href'])[2:-5] + "fullstats/"
+#     game_response = requests.get(game_url, headers=headers)
+#     game_soup = BeautifulSoup(game_response.text, 'html.parser')
+#     game_table = game_soup.find(class_ = "completestats")
 
-    current_scores = []
-    #Name
-    for name in game_table.contents[1].contents[1:]:
-      name = name.text
-      if name not in all_players:
-        all_players[name] = Player(name)
+#     current_scores = []
+#     #Name
+#     for name in game_table.contents[1].contents[1:]:
+#       name = name.text
+#       if name not in all_players:
+#         all_players[name] = Player(name)
         
-      if all_players[name].scores[week] == None:
-        all_players[name].scores[week] = Score()
+#       if all_players[name].scores[week] == None:
+#         all_players[name].scores[week] = Score()
       
-      score = all_players[name].scores[week]
-      current_scores.append(score)
+#       score = all_players[name].scores[week]
+#       current_scores.append(score)
 
-    #Kills
-    for i in range(10):
-      kills = game_table.contents[4].contents[i+1].text
-      current_scores[i].kills += int(kills)
+#     #Kills
+#     for i in range(10):
+#       kills = game_table.contents[4].contents[i+1].text
+#       current_scores[i].kills += int(kills)
 
-    #Deaths
-    for i in range(10):
-      deaths = game_table.contents[5].contents[i+1].text
-      current_scores[i].deaths += int(deaths)
+#     #Deaths
+#     for i in range(10):
+#       deaths = game_table.contents[5].contents[i+1].text
+#       current_scores[i].deaths += int(deaths)
 
-    #Assists
-    for i in range(10):
-      assists = game_table.contents[6].contents[i+1].text
-      current_scores[i].assists += int(assists)
+#     #Assists
+#     for i in range(10):
+#       assists = game_table.contents[6].contents[i+1].text
+#       current_scores[i].assists += int(assists)
 
-    #CS
-    for i in range(10):
-      cs = game_table.contents[8].contents[i+1].text
-      current_scores[i].cs += int(cs)
+#     #CS
+#     for i in range(10):
+#       cs = game_table.contents[8].contents[i+1].text
+#       current_scores[i].cs += int(cs)
 
-    #Triples
-    for i in range(10):
-      triples = game_table.contents[35].contents[i+1].text
-      current_scores[i].triples += int(triples)
+#     #Triples
+#     for i in range(10):
+#       triples = game_table.contents[35].contents[i+1].text
+#       current_scores[i].triples += int(triples)
 
-    #Quadras
-    for i in range(10):
-      quadras = game_table.contents[36].contents[i+1].text
-      current_scores[i].quadras += int(quadras)
+#     #Quadras
+#     for i in range(10):
+#       quadras = game_table.contents[36].contents[i+1].text
+#       current_scores[i].quadras += int(quadras)
 
-    #Pentas
-    for i in range(10):
-      pentas = game_table.contents[37].contents[i+1].text
-      current_scores[i].pentas += int(pentas)
+#     #Pentas
+#     for i in range(10):
+#       pentas = game_table.contents[37].contents[i+1].text
+#       current_scores[i].pentas += int(pentas)
 
-    #Update totals
-    for s in current_scores:
-      s.games += 1
-      s.update()
-
-
+#     #Update totals
+#     for s in current_scores:
+#       s.games += 1
+#       s.update()
 
 
-# Application Default credentials are automatically created.
-app = firebase_admin.initialize_app() #https://firebase.google.com/docs/admin/setup#python_2 has info on users
-db = firestore.client()
 
-#convert string to unicode
-def to_uni(s:str):
-  return (re.sub('.', lambda x: r'\u % 04X' % ord(x.group()), s))
 
-#write info into database
-col_ref = db.collection(u'Players')
-for p in all_players:
-  player = all_players[p] #get player object
+# # Application Default credentials are automatically created.
+# app = firebase_admin.initialize_app() #https://firebase.google.com/docs/admin/setup#python_2 has info on users
+# db = firestore.client()
 
-  #convert name to unicode
-  uni_name = to_uni(p)
-  col_ref.document(uni_name).set({
-    u'name':uni_name,
-    u'team':to_uni(player.team)
+# #convert string to unicode
+# def to_uni(s:str):
+#   return (re.sub('.', lambda x: r'\u % 04X' % ord(x.group()), s))
+
+# #write info into database
+# col_ref = db.collection(u'Players')
+# for p in all_players:
+#   player = all_players[p] #get player object
+
+#   #convert name to unicode
+#   uni_name = to_uni(p)
+#   col_ref.document(uni_name).set({
+#     u'name':uni_name,
+#     u'team':to_uni(player.team)
     
 
-  })
-  break
+#   })
+#   break
 
 
 # The `project` parameter is optional and represents which project the client
